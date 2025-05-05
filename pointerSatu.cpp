dfabf2dd1c08568d44b4f06f5378b78ec71d7828
PasyaMaulana <pasyamaulana17@gmail.com>
2025-05-05 10:48:24 +0700

    pointer reference

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -15,4 +15,8 @@ int main()
 {
     mahasiswa mhs{1}; // Object mhs
     mhs.showNim();    // Member Access Operator
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2; // Member Access Operator
+    mhs.showNim();
 }
